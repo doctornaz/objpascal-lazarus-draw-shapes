dc492b78f13998c16fef35e992bd3184a59a567f
Jorge Sarmiento <doctor.n4z@gmail.com>
2015-05-23 19:04:06 -0430

    Star is now drawn upside down. Added the colored points for the challenge.

--- unit1.pas
+++ unit1.pas
@@ -12,10 +12,12 @@ type
   { TMain }
 
   TMain = class(TForm)
+    Pintar: TButton;
     Skeleton: TImage;
     Pentagrama: TButton;
     Salir: TButton;
     procedure PentagramaClick(Sender: TObject);
+    procedure PintarClick(Sender: TObject);
     procedure SalirClick(Sender: TObject);
   private
 
@@ -46,17 +48,43 @@ begin
   Canvas.Pen.Color := clRED;
   Canvas.Pen.Width:= 3;
   Canvas.Font.Color := clRED;
+  Canvas.Font.Size:= 20;
 
   Canvas.Ellipse(Width DIV 2 -200 , Height DIV 2 - 200, Width DIV 2 + 200, Height DIV 2 + 200);
 
-  Canvas.MoveTo(Width DIV 2, Height DIV 2 -200);
-  Canvas.LineTo(Width DIV 2 - 120, Height DIV 2 + 160);
-  Canvas.LineTo(Width DIV 2 + 185, Height DIV 2 - 70);
-  Canvas.LineTo(Width DIV 2 - 185, Height DIV 2 - 70);
-  Canvas.LineTo(Width DIV 2 + 120, Height DIV 2 + 160);
-  Canvas.LineTo(Width DIV 2, Height DIV 2 -200);
+  Canvas.MoveTo(Width DIV 2, Height DIV 2 +200);
+  Sleep(500);
+  Canvas.LineTo(Width DIV 2 + 120, Height DIV 2 - 160);
+  Sleep(500);
+  Canvas.LineTo(Width DIV 2 - 185, Height DIV 2 + 70);
+  Sleep(500);
+  Canvas.LineTo(Width DIV 2 + 185, Height DIV 2 + 70);
+  Sleep(500);
+  Canvas.LineTo(Width DIV 2 - 120, Height DIV 2 - 160);
+  Sleep(500);
+  Canvas.LineTo(Width DIV 2, Height DIV 2 +200);
+  Sleep(500);
+  Canvas.TextOut(Width DIV 2 -45, Height DIV 2, 'Spooky');
 
-  Canvas.TextOut(Width DIV 2 -20, Height DIV 2 - 20, 'Spooky');
+  Pintar.Enabled:= true;
+end;
+
+procedure TMain.PintarClick(Sender: TObject);
+begin
+  Canvas.Brush.Color:= $660066;
+  Canvas.FloodFill(Width DIV 2, Height DIV 2 +100, clRED, fsBorder);
+  Sleep(500);
+  Canvas.Brush.Color:= $00FF00;
+  Canvas.FloodFill(Width DIV 2 +100, Height DIV 2 -140, clRED, fsBorder);
+  Sleep(500);
+  Canvas.Brush.Color:= $00FFFF;
+  Canvas.FloodFill(Width DIV 2 -80, Height DIV 2 +40, clRED, fsBorder);
+  Sleep(500);
+  Canvas.Brush.Color:= $FF66FF;
+  Canvas.FloodFill(Width DIV 2 +80, Height DIV 2 +40, clRED, fsBorder);
+  Sleep(500);
+  Canvas.Brush.Color:= $FFFF00;
+  Canvas.FloodFill(Width DIV 2 -100, Height DIV 2 -140, clRED, fsBorder);
 end;
 
 end.
